40827ca566cf83483bd96927c242ecadeab6f12a
Administrator <494179456@qq.com>
2017-04-13 23:18:28 +0800

    android

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Typeface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -19,6 +21,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,6 +31,17 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.Legend;
+import com.github.mikephil.charting.utils.Legend.LegendForm;
+import com.github.mikephil.charting.utils.XLabels;
+import com.github.mikephil.charting.utils.XLabels.XLabelPosition;
+import com.github.mikephil.charting.utils.YLabels;
+
+
 import org.apache.http.util.EncodingUtils;
 
 import java.io.FileInputStream;
@@ -35,33 +49,61 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import lecho.lib.hellocharts.gesture.ContainerScrollType;
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
+
 
 public class MainActivity extends AppCompatActivity implements
         SensorEventListener,View.OnClickListener {
     private SensorManager sensorManager;
-    private boolean doWrite = true;
+    private boolean flag = true;
     private Button start, stop, show, map;
     TextView etTxt1;
     private float xValue, yValue, zValue;
-    private float oxValue=0;
-    private float oyValue=0;
-    private float ozValue=0;
-    private int x = 0;
+    private int j=0;
 
-    private SurfaceView mSurfaceView = null;
-    private SurfaceHolder mSurfaceHolder = null;
-    private Paint mPaint = null;
 
+    private LineChartView lineChart1;
+    private LineChartView lineChart2;
+    private LineChartView lineChart3;
+
+    private List<PointValue> xPointValues = new ArrayList<PointValue>();
+    private List<PointValue> yPointValues = new ArrayList<PointValue>();
+    private List<PointValue> zPointValues = new ArrayList<PointValue>();
+    private List<AxisValue> mAxisXValues = new ArrayList<AxisValue>();
+
+    String[] date =new String[100];
+    int[] score=new  int[100];
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         etTxt1 = (TextView) findViewById(R.id.shuju);
         sensorManager = (SensorManager) getSystemService(
                 Context.SENSOR_SERVICE);
+
+//        long Time= System.currentTimeMillis();
+//        int savedTime=(int)Time;
+
+        lineChart1=(LineChartView) findViewById(R.id.chart1);
+        lineChart2=(LineChartView) findViewById(R.id.chart2);
+        lineChart3=(LineChartView) findViewById(R.id.chart3);
+
 
         Button start = (Button) findViewById(R.id.start);
         start.setOnClickListener(this);
@@ -71,22 +113,56 @@ public class MainActivity extends AppCompatActivity implements
         show.setOnClickListener(this);
         Button map = (Button) findViewById(R.id.map);
         map.setOnClickListener(this);
-        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
-        mSurfaceView.setZOrderOnTop(true);
-        mSurfaceView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
-        mSurfaceHolder = mSurfaceView.getHolder();
-        mSurfaceHolder.addCallback(new MyHolder());
-
-//        init();
     }
 
 
-    private void init(){
-        mPaint = new Paint();
-        mPaint.setColor(Color.BLACK);
-        mPaint.setStrokeWidth(1);
-        mPaint.setStyle(Paint.Style.FILL);
-    }
+    private void initLineChart(LineChartView lineChart,List<PointValue> pointValues,int color){
+          Line line=new Line(pointValues).setColor(color).setStrokeWidth(2);
+          List<Line> lines=new ArrayList<Line>();
+          line.setShape(ValueShape.CIRCLE);//折线图上每个数据点的形状  这里是圆形 （有三种 ：ValueShape.SQUARE  ValueShape.CIRCLE  ValueShape.DIAMOND）
+          line.setCubic(true);//曲线是否平滑，即是曲线还是折线
+          line.setFilled(false);//是否填充曲线的面积
+          line.setHasLabels(false);//曲线的数据坐标是否加上备注
+//        line.setHasLabelsOnlyForSelected(true);//点击数据坐标提示数据（设置了这个line.setHasLabels(true);就无效）
+          line.setHasLines(true);//是否用线显示。如果为false 则没有曲线只有点显示
+          line.setHasPoints(false);//是否显示圆点 如果为false 则没有原点只有点显示（每个数据点都是个大的圆点）
+          lines.add(line);
+          LineChartData data = new LineChartData();
+          data.setLines(lines);
+
+          Axis axisX=new Axis();
+          axisX.setHasTiltedLabels(false);  //X坐标轴字体是斜的显示还是直的，true是斜的显示
+          axisX.setTextColor(Color.WHITE);  //设置字体颜色
+          axisX.setTextColor(Color.parseColor("#336699"));
+          axisX.setName("加速度值");  //表格名称
+          axisX.setTextSize(10);//设置字体大小
+          axisX.setMaxLabelChars(8); //最多几个X轴坐标，意思就是你的缩放让X轴上数据的个数7<=x<=mAxisXValues.length
+          axisX.setValues(mAxisXValues);  //填充X轴的坐标名称
+          data.setAxisXBottom(axisX); //x 轴在底部
+          //data.setAxisXTop(axisX);  //x 轴在顶部
+          axisX.setHasLines(true); //x 轴分割线
+
+          Axis axisY = new Axis();  //Y轴
+          axisY.setName("m/s^2");//y轴标注
+          axisY.setTextSize(10);//设置字体大小
+          axisY.setTextColor(Color.WHITE);  //设置字体颜色
+          axisY.setTextColor(Color.parseColor("#336699"));
+          data.setAxisYLeft(axisY);  //Y轴设置在左边
+
+          //设置行为属性，支持缩放、滑动以及平移
+          lineChart.setInteractive(true);
+          lineChart.setZoomType(ZoomType.HORIZONTAL);
+          lineChart.setMaxZoom((float) 2);//最大方法比例
+          lineChart.setContainerScrollEnabled(true, ContainerScrollType.HORIZONTAL);
+          lineChart.setLineChartData(data);
+          lineChart.setVisibility(View.VISIBLE);
+
+          Viewport v=new Viewport(lineChart.getMaximumViewport());
+          v.left=0;
+          lineChart.setCurrentViewport(v);
+
+}
+
 
     @Override
     protected void onResume() {
@@ -119,11 +195,11 @@ public class MainActivity extends AppCompatActivity implements
 
         if (view.getId() == R.id.start) {
             onResume();
-            doWrite = true;
+            flag = true;
         }
         if (view.getId() == R.id.stop) {
             onStop();
-            doWrite = false;
+            flag = false;
         }
         if (view.getId() == R.id.show) {
             Toast.makeText(MainActivity.this, "show data", Toast.LENGTH_SHORT).show();
@@ -152,72 +228,25 @@ public class MainActivity extends AppCompatActivity implements
         message += df.format(yValue) + " ";
         message += df.format(zValue) + " ";
         etTxt1.setText(message + "\n");
-        if (doWrite) {
+        if (flag) {
             writeFileData("acc.txt", message);
+
+            mAxisXValues.add(new AxisValue(j).setLabel(String.valueOf(j)));
+            xPointValues.add(new PointValue(j,xValue));
+            initLineChart(lineChart1,xPointValues,Color.rgb(137, 230, 81));
+            mAxisXValues.add(new AxisValue(j).setLabel(String.valueOf(j)));
+            yPointValues.add(new PointValue(j,yValue));
+            initLineChart(lineChart2,yPointValues,Color.rgb(240, 240, 30));
+            mAxisXValues.add(new AxisValue(j).setLabel(String.valueOf(j)));
+            zPointValues.add(new PointValue(j,zValue));
+            initLineChart(lineChart3,zPointValues,Color.rgb(89, 199, 250));
+            j++;
         }
 
-//        Canvas mCanvas = mSurfaceHolder.lockCanvas();
-//        DisplayMetrics dm=new DisplayMetrics();
-//        getWindowManager().getDefaultDisplay().getMetrics(dm);
-//        int height=dm.heightPixels;
-//        int width=dm.widthPixels;
-//        mPaint.setColor(Color.BLACK);
-//        mCanvas.drawLine(0,0,0,height,mPaint);
-//        mCanvas.drawLine(0,50,width,50,mPaint);
-//        mCanvas.drawLine(0,150,width,150,mPaint);
-//        mCanvas.drawLine(0,250,width,250,mPaint);
-//        try {
-//            if (mCanvas != null) {
-//                mPaint.setColor(Color.RED);
-//                //mCanvas.drawPoint(x,50 + xValue,mPaint);
-//                mCanvas.drawLine(x,50-oxValue,x+1,50-xValue,mPaint);
-//                mPaint.setColor(Color.GREEN);
-//                //mCanvas.drawPoint(x,150 + yValue,mPaint);
-//                mCanvas.drawLine(x,150-oyValue,x+1,150-yValue,mPaint);
-//                mPaint.setColor(Color.BLUE);
-//                //mCanvas.drawPoint(x,250 + zValue,mPaint);
-//                mCanvas.drawLine(x,250-ozValue,x+1,250-zValue,mPaint);
-//                x++;
-//                oxValue=xValue;
-//                oyValue=yValue;
-//                ozValue=zValue;
-//                mSurfaceHolder.unlockCanvasAndPost(mCanvas);
-//                if (x > width) {
-//                    x = 0;
-//                    mCanvas=mSurfaceHolder.lockCanvas(new Rect(0,height,width,0));
-//                    mCanvas.drawColor(Color.TRANSPARENT,PorterDuff.Mode.CLEAR);
-//                    mSurfaceHolder.unlockCanvasAndPost(mCanvas);
-//                }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (mCanvas != null) {
-//                mSurfaceHolder.lockCanvas(new Rect(0, 0, 0, 0));
-//                mSurfaceHolder.unlockCanvasAndPost(mCanvas);
-//            }
-//        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    }
-
-
-
-    public class MyHolder implements SurfaceHolder.Callback{
-
-        @Override
-        public void surfaceCreated(SurfaceHolder holder) {
-        }
-
-        @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        }
-
-        @Override
-        public void surfaceDestroyed(SurfaceHolder holder) {
-        }
     }
 }
 
